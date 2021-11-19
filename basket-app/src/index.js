e99abb26c7f3755746c847b981448893b153fc00
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-11-19 13:28:28 +0200

    # Create general main component

--- basket-app/src/index.js
+++ basket-app/src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Header } from './js/Header';
+import { Main } from './js/Main';
 
 ReactDOM.render(
   <React.StrictMode>
     <Header />
+    <Main />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ basket-app/src/js/Main.js
@@ -0,0 +1,12 @@
+import { Component } from 'react';
+
+export class Main extends Component {
+  render() {
+    return (
+      <main>
+        <h2> Groceries List </h2> {/* TODO - replace with component - GroceriesList*/}
+        <h2> Basket List </h2> {/* TODO - replace with component - BasketList*/}
+      </main>
+    );
+  }
+}
