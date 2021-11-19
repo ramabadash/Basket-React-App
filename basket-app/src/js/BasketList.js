94dc5b7b189085d4870d59887e1d5a7293ace933
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-11-19 17:42:20 +0200

    # Add a list base on the "generateItemsToBasket" function
    The function takes the state. According to it constructs li-type objects that represent the current state.

--- basket-app/src/js/BasketList.js
+++ basket-app/src/js/BasketList.js
@@ -12,6 +12,22 @@ export class BasketList extends Component {
           <i className='fas fa-shopping-basket logo'></i>
           Basket:
         </h2>
+        <ul className='basket-list'>{this.generateItemsToBasket()}</ul>
       </>
     );
   }
+  generateItemsToBasket() {
+    const itemsArr = [];
+    const basketItems = this.state.basketItems;
+    for (const item in basketItems) {
+      itemsArr.push(
+        //TODO - replace to BasketItem component
+        <li key={item}>
+          <i className='far fa-minus-square'></i>
+          {basketItems[item]} {item}
+        </li>
+      );
+    }
+    return itemsArr;
+  }
+}
